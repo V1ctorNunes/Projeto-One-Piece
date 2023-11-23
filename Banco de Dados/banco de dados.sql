7f1d2fc1f5c7c92a4b90445f6830b5d7d3fce07f
V1ctorNunes <victor.moreira@sptech.school>
2023-11-23 14:38:45 -0300

    Atualização 2311

--- Banco de Dados/banco de dados.sql
+++ Banco de Dados/banco de dados.sql
@@ -9,7 +9,24 @@ email varchar(120),
 senha char(45)
 );
 
+create table perfil (
+idPerfil int auto_increment,
+nickname varchar(100),
+score int,
+tentativas int,
+fkUsuario int,
+foreign key (fkUsuario) references usuario(idUsuario),
+primary key (idPerfil, fkUsuario)
+);
+
 select * from usuario;
+
+select idUsuario from usuario;
+
+truncate table usuario;
+delete from usuario where idusuario = 1;
+drop table ranking;
+drop table usuario;
 
 create table ranking (
 idQuiz int primary key auto_increment,
@@ -22,25 +39,40 @@ acertos int
 
 select * from ranking;
 
-SELECT usuario.nickname AS nickname, SUM(score) AS score
-      FROM ranking
-      JOIN usuario ON fkUsuario = idUsuario
-      GROUP BY fkUsuario
-      ORDER BY score DESC;
+delete from ranking where idQuiz = 1;
+delete from ranking where fkUsuario = 6;
+
+
+truncate table ranking;
       
 SELECT usuario.nickname AS Nickname, 
 	ranking.tentativas AS Tentativas,
     ranking.score AS Pontos,
     ranking.acertos AS Acertos
 	from ranking join usuario
-    on ranking.fkUsuario = usuario.idUsuario;
+    on ranking.fkUsuario = usuario.idUsuario
+	order by ranking.score desc;
       
+insert into usuario values 
+	(null, 'Victor', 'Vitin', 'vitin@hotmail.com', '1234');
+
+insert into usuario values 
+	(null, 'Thiago', 'Tigas', 'tigas@hotmail.com', '1234'),
+    (null, 'Vitor', 'Hugo', 'hugo@hotmail.com', '1234'),
+    (null, 'Guto', 'Gutin', 'gutin@hotmail.com', '1234'),
+	(null, 'Carmona', 'carminha', 'carminhan@hotmail.com', '1234');
+    
+
       
 insert into ranking values 
-(1, '2', '3', '115', '3');
+(1, '1', '3', '115', '3'),
+(2, '2', '1', 76, '2'),
+(3, '3', '2', '115', '3'),
+(4, '4', '3', '115', '3'),
+(5, '5', '1', '55', '1');
 
 insert into ranking values 
-(null, 3, 3, 115, 3);
+	(1, '1', '3', '115', '3');
 
 create table historico_tentativas (
     idHistorico int primary key auto_increment,
